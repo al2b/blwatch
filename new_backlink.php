ad2032973998db7fc30f8a4938cda78ee078101c
al2b <deboissieu.al@gmail.com>
2017-05-26 11:57:42 +0200

    fix template

--- new_backlink.php
+++ new_backlink.php
@@ -2,6 +2,30 @@
 require("lib/deals.php");
 $identity["id"] = $_REQUEST["id"] ;
 $deal = deal_by_id($identity["id"]) ;
+?>
 
-include('tpl/deals/form-new-backlink.html') ;
-echo "quoi? ";
+<!DOCTYPE html>
+<html lang="fr">
+
+      <?php  include('tpl/general/header.php') ;?>
+
+
+      <body>
+
+          <div id="wrapper">
+              <?php include('tpl/general/menu.html') ; ?>
+              <!-- Page Content -->
+              <div id="page-content-wrapper">
+                  <div class="container-fluid">
+                      <div class="row">
+                          <div class="col-lg-12">
+                              <h1>Well done. Maintenant, ajoutez vos backlinks</h1>
+              <?php  include('tpl/deals/form-new-backlink.html') ;?>
+</div>
+</div>
+</div>
+</div>
+<!-- /#page-content-wrapper -->
+</div>
+</body>
+</html>
