8709a89c6bb51582a3e236439a5b4b974876cad4
al2b <deboissieu.al@gmail.com>
2017-06-01 22:47:06 +0200

    fix body.html

--- new_backlink.php
+++ new_backlink.php
@@ -22,52 +22,10 @@ if (array_key_exists("backlink", $_REQUEST)) {
 
 $identity["id"] = $_REQUEST["id"] ;
 $deal = deal_by_id($identity["id"]) ;
+
+$h1 = "Well done. Maintenant, ajoutez vos backlinks" ;
+$texte = "Ce formulaire vous permet d'enregistrer un de vos contacts. Vous pourrez associer les infos sur les liens de votre choix juste après, ou bien plus tard.";
+$display_backlinks = DISPLAY_BACKLINKS;
+$form = FORM_NEW_BACKLINK;
+include('tpl/general/body.html') ;
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-
-      <?php  include('tpl/general/header.php') ;?>
-
-
-      <body>
-
-          <div id="wrapper">
-              <?php include('tpl/general/menu.html') ; ?>
-              <!-- Page Content -->
-              <div id="page-content-wrapper">
-                  <div class="container-fluid">
-                      <div class="row">
-                          <div class="col-lg-12">
-                              <h1>Well done. Maintenant, ajoutez vos backlinks</h1>
-                              <table class="table table-striped table-hover ">
-
-                                  <thead>
-                                    <tr>
-                                      <th>Date</th>
-                                      <th>URL</th>
-                                      <th>Cible</th>
-                                      <th>Prix</th>
-                                      </tr>
-                                    </thead>
-                                    <tbody>
-                                      <tr>
-                                        <?php foreach ($deal["backlinks"] as $key => $backlink) { ?>
-                                          <td><?php echo $backlink["date"]; ?></td>
-                                          <td><?php echo $backlink["url"]; ?></td>
-                                          <td><?php echo $backlink["target"]; ?></td>
-                                          <td><?php echo $backlink["price"]; ?></td>
-                                        </tr>
-                                      </tbody>
-                                      <?php } ?>
-                                    </table>
-                                  <p> Total du coût des backlinks pour ce deal : <?php echo $somme = somme_deal($deal) ?> euros</p>
-              <?php  include('tpl/deals/form-new-backlink.html') ;?>
-</div>
-</div>
-</div>
-</div>
-<!-- /#page-content-wrapper -->
-</div>
-</body>
-</html>
