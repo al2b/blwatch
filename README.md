174bcef42ee4299cd7bba1381adbbc4f0793b6f5
al2b <deboissieu.al@gmail.com>
2017-06-27 14:18:27 +0200

    passage symfony et ajout deal et suppression fichier

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# blwatch
+projetnew
+=========
+
+A Symfony project created on June 15, 2017, 12:32 pm.
--- new_display_deal.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo "vous cherchez le deal suivant :";
-include('tpl/deals/form-show-id.html') ;
